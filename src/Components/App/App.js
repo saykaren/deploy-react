b5301596a13dc12f9c36126e8372a3d5b7fe3230
Karen Garcia <karen.garcia@nexscreen.com>
2019-08-04 20:37:22 -0600

    adding details to package.json hoping for deployment to work

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,12 +2,25 @@ import React from 'react';
 import './App.css';
 import NavigationBar from './Navigation';
 import Footer from './Footer1';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import Home from './HomeSection';
+import ContactMe from './ContactMe';
 
 const App = () => (
-    <div>
-      <NavigationBar />
-      <Footer />
-    </div>
+    <Router basename='/'>
+    <NavigationBar />
+      <div>
+        <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        </ul>
+    <hr />
+    <Route exact path="/" component={Home} />
+    <Route path="/about" component={ContactMe} />
+   </div>
+   <Footer/>
+
+    </Router>
   );
 
 export default App;
